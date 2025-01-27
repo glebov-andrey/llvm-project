42c8e5637bbcfc8f0e5da76ad14d6c6b0d14559c
Andrey Glebov <andrey458641387@gmail.com>
2025-01-28 01:34:15 +0300

    [clang] [WIP] MicrosoftCXXABI: save and load the RecordToCopyCtor table (#53486)
    - Replaces the previous attempt at restoring the table
    - The implementation is currently broken

--- clang/lib/AST/ASTContext.cpp
+++ clang/lib/AST/ASTContext.cpp
@@ -13116,6 +13116,11 @@ ASTContext::createMangleNumberingContext() const {
   return ABI->createMangleNumberingContext();
 }
 
+const llvm::SmallDenseMap<CXXRecordDecl *, CXXConstructorDecl *> *
+ASTContext::getRecordToCopyCtor() const {
+  return ABI->getRecordToCopyCtor();
+}
+
 const CXXConstructorDecl *
 ASTContext::getCopyConstructorForExceptionObject(CXXRecordDecl *RD) {
   return ABI->getCopyConstructorForExceptionObject(
